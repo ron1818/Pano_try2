b6098fbe4aa6666126922b9560cb1ca36208487e
Ye <ye.ren@rolls-royce.com>
2020-12-23 21:56:47 +0800

    [Fix] change java module to android 4.2.0

--- app/src/main/java/com/example/try2/StitchProcess.java
+++ app/src/main/java/com/example/try2/StitchProcess.java
@@ -4,19 +4,24 @@ import android.graphics.Bitmap;
 
 import org.opencv.calib3d.Calib3d;
 import org.opencv.core.Mat;
+import org.opencv.core.Size;
 import org.opencv.features2d.FlannBasedMatcher;
 import org.opencv.features2d.ORB;
 import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
 
 public class StitchProcess implements ImageStitchNative.onStitchResultListener {
     private Mat mat1, mat2;
     private String p1, p2;
 
     public StitchProcess(String path1, String path2){
-        mat1 = Imgcodecs.imread(path1);
-        mat2 = Imgcodecs.imread(path2);
-        p1 = path1;
-        p2 = path2;
+        Mat _mat1 = Imgcodecs.imread(path1);
+        Mat _mat2 = Imgcodecs.imread(path2);
+        mat1 = new Mat();
+        mat2 = new Mat();
+        // make mat1 and mat2 smaller
+        Imgproc.resize(_mat1, mat1, new Size(0,0), 0.25, 0.25);
+        Imgproc.resize(_mat2, mat2, new Size(0,0), 0.25, 0.25);
     }
 
     public void Stitch(){
